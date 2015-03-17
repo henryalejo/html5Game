65d943dff4a7c1b3d87602085ff01619f730d4f9
root <henryalejo@gmail.com>
2015-03-16 23:38:02 -0500

    sounds

--- js/angularquintus.js
+++ js/angularquintus.js
@@ -2,8 +2,8 @@
     var app = angular.module('Quintus',[])
 	    	.provider('Q', function(){
 	      	
-		        var instance = Quintus();
-
+		        var instance = Quintus({audioSupported: ['mp3','wav']});
+		        //var instance = Quintus();
 		        this.include = function(array) {
 		          instance.include(array.join(','));
 		        };
@@ -15,6 +15,9 @@
 		        };
 		        this.touch = function(touch) {
 		          instance.touch(touch);
+		        };
+		        this.enableSound = function(sound) {
+		          instance.enableSound(sound);
 		        };
 
 		        this.$get = function() {
--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@
     
 
     .config(function(QProvider) {
-		QProvider.include(['Sprites','Scenes','Input','2D','Touch','UI']);
+     // QProvider.$get({audioSupported: ['wav']});
+		QProvider.include(['Sprites','Scenes','Input','2D','Touch','UI','Audio']);
 		QProvider.setup("myGame");                         // Add a canvas element onto the page
         QProvider.controls();                        // Add in default controls (keyboard, buttons)
-        QProvider.touch();                            // Add in touch support (for the UI)
+        QProvider.touch();
+        QProvider.enableSound();                            // Add in touch support (for the UI)
        	
 
 	});
@@ -23,6 +25,7 @@
 		$scope.lives= new Array(3);
     $scope.maxlevels=4;
     $scope.players=[];
+    $scope.sound=true;
     $http.get('http://localhost:3000/api/players').success(function(data) {
                   console.log('Succes get players'); 
                   console.log(data);
@@ -82,7 +85,8 @@
         init: function(p) {
           this._super(p, { sheet: "player"});
           this.add('2d, platformerControls');
-          
+          this.on("jump",function(obj){ Q.audio.play('../assets/smb_jump-small.mp3');              });
+          //this.on("jump");
           this.on("hit.sprite",function(collision) {
             if(collision.obj.isA("Tower")) {
               $scope.plusScore(100);
@@ -91,10 +95,14 @@
               if($scope.levelnumber < $scope.maxlevels){
                 $scope.levelnumber++;
                 Q.stageScene("nextLevel",1,{ label: "You Won" }); 
+                Q.audio.stop('../assets/map-1.mp3');
+                Q.audio.play('../assets/smb_stage_clear.mp3');
 
               }else{
                  Q.stageScene("youLose",1, { label: "Winner" }); 
                   collision.obj.destroy();
+                  Q.audio.stop('../assets/map-1.mp3');
+                  Q.audio.play('../assets/smb_world_clear.mp3');
               };
             	
               
@@ -106,6 +114,9 @@
             }
           });
         }
+
+        
+
       });
 
       Q.Sprite.extend("Tower", {
@@ -119,18 +130,27 @@
         init: function(p) {
           this._super(p, { sheet: 'enemy', vx: 100 });
           this.add('2d, aiBounce');
+
           
+
           this.on("bump.left,bump.right,bump.bottom",function(collision) {
             if(collision.obj.isA("Player")) { 
 
               if($scope.lives.length>0){
                   $scope.lessLives();
+                  $scope.score=0;
+                  $scope.$apply;
                   Q.stageScene("endGame",1, { label: "You Died" }); 
+
                   collision.obj.destroy();
+                  Q.audio.stop('../assets/map-1.mp3');
+                  Q.audio.play('../assets/smb_mariodie.mp3');
               }
               else{
                   Q.stageScene("youLose",1, { label: "You Lose" }); 
                   collision.obj.destroy();
+                  Q.audio.stop('../assets/map-1.mp3');
+                   Q.audio.play('../assets/smb_gameover.mp3');
               };         	
             	
             }
@@ -139,12 +159,14 @@
           this.on("bump.top",function(collision) {
             if(collision.obj.isA("Player")) { 
               this.destroy();
+              Q.audio.play('../assets/smb_coin.mp3');
               collision.obj.p.vy = -300;
               $scope.plusScore(25);
              
 
             }
           });
+          
         }
       });
 
@@ -249,9 +271,12 @@
             stage.insert(new Q.Enemy({ x: 700, y: 0 }));
             stage.insert(new Q.Enemy({ x: 800, y: 0 }));        
             stage.insert(new Q.Tower(level.tower));
+            Q.audio.stop('../assets/map-1.mp3');
+            if($scope.sound)
+            Q.audio.play('../assets/map-1.mp3',{ loop: true });
           });
 
-          Q.load(['../images/sprites.png', '../data/sprites.json','../images/tiles.png'], function() {      
+          Q.load(['../images/sprites.png', '../data/sprites.json','../images/tiles.png','../assets/map-1.mp3','../assets/smb_jump-small.mp3','../assets/smb_coin.mp3','../assets/smb_stage_clear.mp3','../assets/smb_world_clear.mp3','../assets/smb_gameover.mp3','../assets/smb_mariodie.mp3'], function() {      
           Q.assets['level.json']=level.levelarray;
           //Q.assets['level.json']=$scope.miarray;
           //console.log(level.levelarray);
@@ -264,20 +289,27 @@
         };
 
         $scope.modalScore = function (score) {
-                    ////console.log('entro a edit modal siganture');
-                    $scope.score2=score;
-                    var modalInstance = $modal.open({
-                        templateUrl: 'tpl/addscore.html',
-                        controller: 'ModalAddScore',
-                        size: 'sm'
-                        ,
-                        resolve: {
-                              scoredom: function () {
-                            return $scope.score2;
-                          }
-                        }
-                    });   
-                };
+          ////console.log('entro a edit modal siganture');
+          $scope.score2=score;
+          var modalInstance = $modal.open({
+              templateUrl: 'tpl/addscore.html',
+              controller: 'ModalAddScore',
+              size: 'sm'
+              ,
+              resolve: {
+                    scoredom: function () {
+                  return $scope.score2;
+                }
+              }
+          });   
+        };
+
+        $scope.stopMusic = function(){
+          $scope.sound=!$scope.sound;
+          if(!$scope.sound)
+          Q.audio.stop('../assets/map-1.mp3');
+          else Q.audio.play('../assets/map-1.mp3',{ loop: true });;
+        };
 
 
 
